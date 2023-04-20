807c0ff01cd570d630e98005539726ddd2adb8d5
M Ibrahim U Albab <mibrahimuadev@gmail.com>
2023-04-21 04:00:33 +0700

    init commit

--- src/main/java/com/example/springjwt/SpringjwtApplication.java
+++ src/main/java/com/example/springjwt/SpringjwtApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringjwtApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringjwtApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringjwtApplication.class, args);
+    }
 
 }
